1766de3e526b8085dc67ba56abb078320c7c748e
shehzad <shozejb@gmail.com>
2023-02-10 10:00:07 +0500

    added files

---
+++ ProjectFour/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectFour.Startup))]
+namespace ProjectFour
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
